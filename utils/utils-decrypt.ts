0407992ff9e7c244da79a0d5b08cbb72940923a6
Eduard975 <theblackasasin@yahoo.com>
2025-03-26 15:27:56 +0200

    small fix

--- utils/utils-decrypt.ts
+++ utils/utils-decrypt.ts
@@ -14,14 +14,17 @@ export function inverseShiftRows(matrix: Matrix): Matrix {
     matrix[1],
     matrix[2],
     matrix[3],
+
     matrix[7],
     matrix[4],
     matrix[5],
     matrix[6],
+
     matrix[10],
     matrix[11],
     matrix[8],
     matrix[9],
+    
     matrix[13],
     matrix[14],
     matrix[15],
