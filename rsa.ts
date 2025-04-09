d6f70003c33137a7252cc3889d143fe8c35a29f7
Eduard975 <theblackasasin@yahoo.com>
2025-04-09 15:24:34 +0300

    Add modInverse for calculating d faster

--- rsa.ts
+++ rsa.ts
@@ -17,26 +17,48 @@ function gcd(a: bigint, b: bigint): bigint {
     return b === 0n ? a : gcd(b, a % b);
 }
 
-function isPrime(num: bigint): boolean {
-    if (num < 2n) return false;
-    for (let i = 2n; i * i <= num; i++) {
-        if (num % i === 0n) return false;
+// function isPrime(num: bigint): boolean {
+//     if (num < 2n) return false;
+//     for (let i = 2n; i * i <= num; i++) {
+//         if (num % i === 0n) return false;
+//     }
+//     return true;
+// }
+
+function modInverse(a: bigint, m: bigint): bigint {
+    let m0 = m, x0 = 0n, x1 = 1n;
+
+    if (m === 1n) return 0n;
+    let q = a / m;
+    let t = m;
+
+    while (a > 1n) {
+        q = a / m;
+        t = m;
+        m = a % m;
+        a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
     }
-    return true;
+
+    if (x1 < 0n) x1 += m0;
+
+    return x1;
 }
 
 //FOARTE SLOW TRB SCHIMBAT CUMVA, PROBABIL CU LUT(Look Up Table) SAU CIURUL LUI ERASTOSTENE SAU 2P-Q
-async function getRandomPrime(min_v = 2, max_v = 100000) {
-    const prime = await fastPrime.random.prime(8);
+async function getRandomPrime(bitSize: number = 8) {
+    const prime = await fastPrime.random.prime(bitSize);
     console.log(prime)
     return BigInt(prime);
   }
 
 
-async function generateKeyPair(): Promise<{ publicKey: { e: bigint; n: bigint }; 
+async function generateKeyPair(primeBitSize: number = 8): Promise<{ publicKey: { e: bigint; n: bigint }; 
                                             privateKey: { d: bigint; n: bigint }; }> {
-    const p =  await getRandomPrime();
-    const q =  await getRandomPrime();
+    const p =  await getRandomPrime(primeBitSize);
+    const q =  await getRandomPrime(primeBitSize);
 
     const n = p * q;
     const phi = (p - 1n) * (q - 1n);
@@ -44,11 +66,9 @@ async function generateKeyPair(): Promise<{ publicKey: { e: bigint; n: bigint };
     while (gcd(e, phi) !== 1n) {
         e += 2n;
     }
-    
-    let d = 1n;
-    while ((d * e) % phi !== 1n) {
-        d += 1n;
-    }
+
+    const d = modInverse(e, phi);
+
     return { publicKey: { e, n }, privateKey: { d, n } };
 }
 
@@ -61,7 +81,7 @@ function decryptMessage(encryptedMessage: bigint[], privateKey: { d: bigint; n: 
 }
 
 
-generateKeyPair().then(keys => {
+generateKeyPair(16).then(keys => {
     const publicKey = keys.publicKey;
     const privateKey = keys.privateKey;
 
