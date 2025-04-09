8c1e6fe42d375987a51f54397572599ce3f8da21
Mihalache Mihai <mihai.mihalache2@student.tuiasi.ro>
2025-04-09 15:21:53 +0300

    Add UI stuff to a folder

--- ui/main.ts
+++ ui/main.ts
@@ -1,17 +1,27 @@
 //Initializator pt Electron
 import { app, BrowserWindow } from "electron";
+import { createServer } from "node:net";
+import path from "node:path";
 
 function createWindow() {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
-		// webPreferences: {
-		//   preload: path.join(__dirname, 'preload.js'), // optional
-		// },
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"), // optional
+			// nodeIntegration: true, // Enable Node in renderer.js
+		},
+	});
+
+	win.webContents.on("did-finish-load", () => {
+		win.webContents.send("connection-info", `IP: ${host}, Port: ${port}`);
 	});
 
 	win.loadFile("index.html"); // your UI
 }
+
+export const host = "127.0.0.1";
+export const port = 7070;
 
 app.whenReady().then(() => {
 	createWindow();
